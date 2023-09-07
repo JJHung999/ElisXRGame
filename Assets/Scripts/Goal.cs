881e01e2e8a134e7e94bcfc79e1c35fd3e41585d
JJHung999 <65639615+JJHung999@users.noreply.github.com>
2023-09-07 12:12:29 -0700

    Adjusted SMPL's unity component. And added a Goals Script Structure

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal 
+//Goal system, not a monobehavior
+//Source: https://www.youtube.com/watch?v=up6HcYph_bo&list=PLtEzwdBEwtJfEMI7uL9hCxH1udUYPqEoC
+{
+    //Properties
+    public string Description { get; set;} 
+    //Describes the goal, gets the goal from something, and then set it on the mission panel
+    public bool Completed { get; set;} 
+    //Checks if that goal was complete
+    public int CurrentAmount { get; set;} 
+    //How much of our goal that we currently did
+    public int RequiredAmount { get; set;} 
+    //How much of our goal that we need to do
+
+    public virtual void Init()
+    //Initizialization method
+    {
+        //default Init Stuff
+        //Will have a function for more complex quests
+    }
+
+
+    public void Evaluate()
+    //Function that checks variables CurrentAmount vs RequiredAmount
+    {
+        if (CurrentAmount >= RequiredAmount)
+        {
+            //if True, call for Complete function
+            Complete();
+        }
+    }
+
+    public void Complete()
+    {
+        Completed = true;
+    }
+}
